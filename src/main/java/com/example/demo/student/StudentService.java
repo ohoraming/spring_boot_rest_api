e1ea0128dfb26c85f40a13ca859543c3c6d49387
ohoraming <kminsmile@gmail.com>
2022-06-29 17:55:49 +0900

    DeleteMapping과 예외처리

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -30,4 +30,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " doex not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
