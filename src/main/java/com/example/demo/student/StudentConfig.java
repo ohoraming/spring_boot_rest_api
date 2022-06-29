decefda005d178ec750e33785c766e18e7979d9e
ohoraming <kminsmile@gmail.com>
2022-06-29 17:18:04 +0900

    Database 항목 수정

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
@@ -13,13 +14,22 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            new Student(
-                    1L,
+            Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5),
-                    21
+                    LocalDate.of(2000, Month.JANUARY, 5)
             );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+
         };
     }
 }
